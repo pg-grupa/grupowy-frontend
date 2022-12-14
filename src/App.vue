f39ca24cb93e90309795052008577e6819a19f3f
Dawid <dawidklose568@gmail.com>
2022-12-14 17:33:11 +0100

    Add routing

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,61 @@
-<script setup lang="ts">
-import MapPreview from "@/components/MapPreview.vue";
-import type { LatLngExpression } from "leaflet";
-
-function onMove(center: LatLngExpression) {
-  console.log(center);
-}
-</script>
 
 <template>
-  <map-preview
-    id="map"
-    :center="[54.372158, 18.638306]"
-    :zoom="12"
-    @moveend="onMove"
-  />
+  <div id="app">
+    <div :id="mapId" class="map"></div>
+    <LRoutingMachine :mapObject="mapObject" :waypoints="waypoints" />
+  </div>
 </template>
 
-<style scoped>
-#map {
-  width: 100vw;
-  height: 100vh;
+<script>
+import LRoutingMachine from "./components/LRoutingMachine.vue";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const waypoints = [
+  { lat: 54.352024, lng: 18.646639 },
+  { lat: 54.372324, lng: 18.646649 },
+];
+
+export default {
+  components: {
+    LRoutingMachine,
+  },
+  data() {
+    return {
+      mapId: "map",
+      mapObject: null,
+      zoom: 6,
+      center: { lat: 54.352024, lng: 18.646639},
+      osmUrl: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      waypoints,
+    };
+  },
+  mounted() {
+    this.mapObject = L.map(this.mapId, {
+      zoom: this.zoom,
+      center: this.center,
+    });
+
+    L.tileLayer(this.osmUrl, {
+      attribution: this.attribution,
+    }).addTo(this.mapObject);
+  },
+};
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
 </style>
